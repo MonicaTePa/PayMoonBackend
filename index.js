4d0ba337ce69440edc3f893873f2fff0aff08ea0
MonicaTePa <moniktepamatp@gmail.com>
2022-03-19 09:19:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const body_parser = require("body-parser");
+//http://localhost: o http://127.0.0.1:
+//const routerApi = require('./routes');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(body_parser.json());
+app.use(express.json());
+
+//routerApi(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
