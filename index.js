95446ac3671d40749a62c1db20c17336255b585f
jlospino10 <piruprohacking@gmail.com>
2022-03-21 18:27:33 -0500

    rutas listas

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require("express");
-const body_parser = require("body-parser");
-const connect_db = require('./config/database');
-const routerApi = require('./routes/index');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const PORT = 3000;
+const conectar_DB = require('./config/database');
+const cors = require('cors');
+const ruta= require('./routes/routes');
 
-const app = express();
+var app = express();
+conectar_DB();
+app.use(cors());
 
-app.use(body_parser.json());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.use('/api', ruta);
+  
+app.listen(3000, () => {
+  console.log("Se ejecuta en el puerto 3000")
+})
 
-connect_db();
-
-routerApi(app);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+module.exports = app;
